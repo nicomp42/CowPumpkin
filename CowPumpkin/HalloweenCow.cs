57c2fcb02878d9d124d8fc5b70ef2c32af0e9d95
Brad <bruceba@mail.uc.edu>
2017-11-02 16:27:57 -0400

    added methods to fix interface

--- CowPumpkin/HalloweenCow.cs
+++ CowPumpkin/HalloweenCow.cs
@@ -2,6 +2,7 @@
  * A halloween-themed demostration of interfaces
  * Bill Nicholson 
  * nicholdw@ucmail.uc.edu
+ *  * edited by Bradley Bruce
  * *******************************************************/
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,18 @@ namespace CowPumpkin {
     class HalloweenCow : Cow, Pumpkin {
         public HalloweenCow(Breed breed) : base(breed) {
 
+        }
+
+
+        //added these methods to fix the missing methods from pumkin
+        public void Carve(string shape)
+        {
+            Console.WriteLine("HalloweenCow is now Carved");
+        }
+
+        public void Decompose()
+        {
+            Console.WriteLine("HalloweenCow is now a lump of mush");
         }
     }
 }
